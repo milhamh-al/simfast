840a29efe46cb8eb5995f7efe0b9814f4ca503bf
Muhammad Ilham Hidayat <mihidayat@alamisharia.co.id>
2022-11-03 11:02:48 +0700

    ref: simplify main alphabet

--- index.js
+++ index.js
@@ -2,14 +2,4 @@ console.log("hello");
 
 console.log("test");
 
-console.log("a")
-
-console.log("a")
-
-console.log("b")
-
-console.log("c")
-
-console.log("d")
-
-console.log("e")
+console.log("this is main alphabet")
